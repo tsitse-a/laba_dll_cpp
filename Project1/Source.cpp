95479ec1805a2e20b355c28497f6f8bffcdff7e0
Цицеровская Алёна <tsitse_a@mail.ru>
2021-05-27 22:39:11 +0500

    Добавлен код

---
+++ Project1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "pch.h"
+
+
+using namespace std;
+
+int main() 
+{
+	char x[50];
+	cout << groupNmber() << "\n";
+	nameStudent(x);
+	cout << x;
+	system("pause");
+}
+ 
---
+++ dinamic/Header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef int groupNmber;
+typedef groupNmber(__stdcall* PgroupNmber) ();
+typedef int nameStudent;
+typedef nameStudent(__cdecl* PnameStudent) (char* PnameStudent);
